5b3f92b00b4d4fe152cb00aaf3581ec8811ca7da
zhoufight <374147621@qq.com>
2018-05-17 23:00:21 +0800

    第一次提交

--- src/components/index.vue
+++ src/components/index.vue
@@ -20,7 +20,7 @@
 						<ul class="nav navbar-nav">
 							<li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
 							<li><a href="#">Link</a></li>
-							
+							<li><a href="#">Link</a></li>
 						</ul>
 					
 					</div><!-- /.navbar-collapse -->
